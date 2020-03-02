1954489a40c71d228b1a22b188012eb08bfdfab8
kolosg <kolos460@gmail.com>
2020-03-02 19:20:48 +0100

    util file updated: + create webdriver method added + read system environment variables method added + possibility to set webdriver properties method added

--- src/main/java/com/kolosg/Jira/testautomation/utility/Util.java
+++ src/main/java/com/kolosg/Jira/testautomation/utility/Util.java
@@ -1,6 +1,50 @@
 package com.kolosg.Jira.testautomation.utility;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class Util {
 
-    // utilities goes here
+    //method simply creates the given webdriver
+    public static WebDriver createDriver(String driverType) {
+        WebDriver driver;
+        if(driverType.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (driverType.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else if (driverType.equalsIgnoreCase("Safari")) {
+            driver = new SafariDriver();
+        } else if (driverType.equalsIgnoreCase("Opera")) {
+            driver = new OperaDriver();
+        } else if (driverType.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else {
+            throw new IllegalArgumentException("No such driver!");
+        }
+        return driver;
+    }
+
+    //method to get pre defined environment variables by key
+    public static String getEnvironmentVariable(String variableName) {
+        String variable = System.getenv(variableName.toUpperCase());
+        if (variable == null) {
+            throw new IllegalArgumentException("Environment variable not found!");
+        }
+        return variable;
+    }
+
+
+    /*
+    //use this method in need to define the driver properties (driverpath + driverproperty)
+    public static void setDriverPath() {
+        String driverProperty = getEnvironmentVariable("DRIVER_PROPERTY");
+        String driverPath = getEnvironmentVariable("DRIVER_PATH");
+        System.setProperty(driverProperty, driverPath);
+    }
+    */
+
 }
