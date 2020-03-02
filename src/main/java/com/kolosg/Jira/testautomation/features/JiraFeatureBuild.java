e80f338a821c397d61156d5a50412bec7768f33d
kolosg <kolos460@gmail.com>
2020-03-02 19:50:43 +0100

    features action build class extended

--- src/main/java/com/kolosg/Jira/testautomation/features/JiraFeatureBuild.java
+++ src/main/java/com/kolosg/Jira/testautomation/features/JiraFeatureBuild.java
@@ -2,19 +2,41 @@ package com.kolosg.Jira.testautomation.features;
 
 import com.kolosg.Jira.testautomation.utility.Util;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
+//action build for features
 public abstract class JiraFeatureBuild {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
 
-    private static final int TIMEOUT_FOR_LOADING = Integer.parseInt(Util.getEnvironmentVariable("timeout_limit"));
-
     protected JiraFeatureBuild(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, TIMEOUT_FOR_LOADING);
+        this.wait = new WebDriverWait(driver, Integer.parseInt(Util.getEnvironmentVariable("timeout_limit")));
     }
 
+    protected void waitUntilElementClickable(WebElement webElement) {
+        wait.until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+
+    protected void waitUntilElementLoaded(WebElement webElement) {
+        wait.until(ExpectedConditions.visibilityOf(webElement));
+    }
+
+    protected void clickOnElement(WebElement webElement) {
+        waitUntilElementClickable(webElement);
+        webElement.click();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void quitDriver() {
+        driver.quit();
+    }
 
 }
