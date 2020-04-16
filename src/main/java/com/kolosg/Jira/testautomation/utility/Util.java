b06bdde9b8140cb587486a989d13888533cfa4c8
kolosg <kolos460@gmail.com>
2020-04-16 22:52:57 +0200

    errorfix

--- src/main/java/com/kolosg/Jira/testautomation/utility/Util.java
+++ src/main/java/com/kolosg/Jira/testautomation/utility/Util.java
@@ -18,14 +18,18 @@ public class Util {
     public final static String USERNAME = getEnvironmentVariable("jira_username");
     public final static String PASSWORD = getEnvironmentVariable("jira_password");
     public final static String BASE_URL = "https://jira.codecool.codecanvas.hu";
-    public static final String GRID_URL = System.getenv("grid_url");
-    public static final String BROWSER = System.getenv("browser");
+    public static final String GRID_URL = getEnvironmentVariable("grid_url");
+    public static final String BROWSER = getEnvironmentVariable("browser");
 
     //method simply creates the given webdriver
     public static WebDriver createDriver() throws MalformedURLException {
         String fullGridUrl = GRID_URL.replace("{PASSWORD}", PASSWORD);
         WebDriver driver;
         MutableCapabilities options = null;
+        System.out.println(BROWSER);
+        System.out.println();
+        System.out.println(BASE_URL);
+        System.out.println(GRID_URL);
         if ("chrome".equals(BROWSER)) {
             options = new ChromeOptions();
         } else if ("firefox".equals(BROWSER)) {
