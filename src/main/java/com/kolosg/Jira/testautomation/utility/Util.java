deacb5c12cd0d7def860e22c7b87fc6a8404bff9
Tomibomi <bothtamas@hotmail.com>
2020-04-28 11:14:30 +0200

    add SELENIUM_GRID password

--- src/main/java/com/kolosg/Jira/testautomation/utility/Util.java
+++ src/main/java/com/kolosg/Jira/testautomation/utility/Util.java
@@ -16,12 +16,13 @@ public class Util {
 
     public final static String USERNAME = System.getenv("DEFAULT_USERNAME");
     public final static String PASSWORD = System.getenv("DEFAULT_PW");
+    public final static String SELENIUM_GRID_PASSWORD = System.getenv("SELENIUM_GRID_PASSWORD");
     public final static String BASE_URL = "https://jira.codecool.codecanvas.hu";
     public final static String GRID_URL = System.getenv("GRID_URL");
     public final static String BROWSER = System.getenv("BROWSER");
 
     public static RemoteWebDriver createDriver() throws MalformedURLException {
-        String fullGridUrl = GRID_URL.replace("{PASSWORD}", PASSWORD);
+        String fullGridUrl = GRID_URL.replace("{PASSWORD}", SELENIUM_GRID_PASSWORD);
         MutableCapabilities options = null;
         System.out.println(fullGridUrl);
         if ("Chrome".equals(BROWSER)) {
